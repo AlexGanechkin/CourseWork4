bca0ddab803b08bc950af03154b3a0a2c7fd3232
AlexGanechkin <terracat80@gmail.com>
2023-01-05 21:00:48 +0400

    Выполнен шаг 3,4 - создали модели и схемвы сериализации

--- dao/model/movie.py
+++ dao/model/movie.py
@@ -11,10 +11,10 @@ class Movie(db.Model):
     trailer = db.Column(db.String(255))
     year = db.Column(db.Integer)
     rating = db.Column(db.Float)
-    # genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
-    # genre = db.relationship("Genre")
-    # director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
-    # director = db.relationship("Director")
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    genre = db.relationship("Genre")
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+    director = db.relationship("Director")
 
 
 class MovieSchema(Schema):
@@ -24,5 +24,5 @@ class MovieSchema(Schema):
     trailer = fields.Str()
     year = fields.Int()
     rating = fields.Float()
-    # genre_id = fields.Int()
-    # director_id = fields.Int()
+    genre_id = fields.Int()
+    director_id = fields.Int()
