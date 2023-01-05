565d8f3eb67b69348f35fd365cfe7f872227ef08
AlexGanechkin <terracat80@gmail.com>
2023-01-05 17:23:31 +0400

    Создали модели и DAO-объекты для представления movie

--- dao/model/movie.py
+++ dao/model/movie.py
@@ -1,10 +1,28 @@
-# здесь модель SQLAlchemy для сущности, также могут быть дополнительные методы работы с моделью (но не с базой, с базой мы работает в классе DAO)
+from marshmallow import Schema, fields
 
 from setup_db import db
 
-class Book(db.Model):
-    __tablename__ = 'book'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    author = db.Column(db.String)
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255))
+    trailer = db.Column(db.String(255))
     year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    genre = db.relationship("Genre")
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+    director = db.relationship("Director")
+
+
+class MovieSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    director_id = fields.Int()
--- dao/movie.py
+++ dao/movie.py
@@ -1,10 +1,46 @@
 # это файл для классов доступа к данным (Data Access Object). Здесь должен быть класс с методами доступа к данным
 # здесь в методах можно построить сложные запросы к БД
+from dao.model.movie import Movie
+
 
 class MovieDAO:
     def __init__(self, session):
         self.session = session
 
-    def get_all_movies(self):
-        movies = Movie.query.all()
-        return
+    def get_all(self):
+        return self.session.query(Movie).all()
+
+    def get_one(self, entity_id):
+        return self.session.query(Movie).get(entity_id)
+
+    def create(self, data):
+        movie = Movie(**data)
+
+        self.session.add(movie)
+        self.session.commit()
+
+        return movie
+
+    def update(self, data):
+        entity_id = data.get('id')
+
+        movie = self.get_one(entity_id)
+
+        movie.title = data.get('title')
+        movie.description = data.get('description')
+        movie.trailer = data.get('trailer')
+        movie.year = data.get('year')
+        movie.rating = data.get('rating')
+        movie.genre_id = data.get('genre_id')
+        movie.director_id = data.get('director_id')
+
+        self.session.add(movie)
+        self.session.commit()
+
+        return movie
+
+    def delete(self, entity_id):
+        movie = self.get_one(entity_id)
+
+        self.session.delete(movie)
+        self.session.commit()
